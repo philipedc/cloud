57a400d5b03102a3f462ac6817173f15584b1b9f
Cesar de Paula Morais <cesarmorais@adimplere.com.br>
2023-11-21 13:56:21 -0300

    Added v1 of recommender

--- TP2/python-docker/README.md
+++ TP2/python-docker/README.md
@@ -1,3 +1,7 @@
 # python-docker
 
 A simple Python app for [Docker's Python Language Guide](https://docs.docker.com/language/python).
+
+# Request Recommendations
+Recommendations can be requested via curl, using command:
+    curl localhost:32211/api/recommend -d '{"songs": [<song_list>]}' -H 'Content-Type: application/json'
--- TP2/python-docker/app.py
+++ TP2/python-docker/app.py
@@ -1,9 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import pickle
+from datetime import date
+
+VERSION = 1.0
+MODEL_DATE = date(2023, 11, 21)
+
 app = Flask(__name__)
+app.model = pickle.load(open("rules/basic_rules.pkl", "rb"))
+
 
 @app.route("/", methods=["GET"])
-def get_rules():
-    return jsonify("Hello World"), 200
+def hello_screen():
+    return jsonify("Hello! Please go to /api/recommend to get a playlist recommendation!"), 200
+
+
+@app.route("/api/recommend", methods=["POST"])
+def get_recommendation():
+    client_songs_list = request.get_json()['songs']
+    recommendation = [
+        rule[1] for rule in app.model if (rule[0] in client_songs_list and rule[2] >= 0.6)
+    ]
+    
+    if len(recommendation) < 5:
+        recommendation = [rule[1] for rule in app.model if (rule[0] in client_songs_list and rule[2] >= 0.4)]
+        
+    # Remove duplicates in recommendation
+    list(set(recommendation))
+    
+    return jsonify(
+        {
+            "playlist_ids":recommendation, 
+            "version": VERSION,
+            "model_date": MODEL_DATE
+        } 
+    )
 
 
 if __name__ == "__main__":
