54508b016eb6835b040e9206935dece307bd2f3b
philipedc <philipedutra2002@gmail.com>
2023-11-26 13:45:20 -0300

    fix server

--- TP2/server/app.py
+++ TP2/server/app.py
@@ -9,23 +9,16 @@ MODEL_DATE = date(2023, 11, 21)
 app = Flask(__name__)
 
 file_path = "/app/rules/basic_rules.pkl"
-LAST_MODIFICATION = os.path.getmtime(file_path)
 app.model = pickle.load(open(file_path, "rb"))
 
-def check_update_file():
-    file_path = "/app/rules/basic_rules.pkl"
-    if (LAST_MODIFICATION != os.path.getmtime(file_path))
-    app.model = pickle.load(open(file_path, "rb"))
 
 @app.route("/", methods=["GET"])
 def hello_screen():
-    check_update_file()
     return jsonify("Hello! Please go to /api/recommend to get a playlist recommendation!"), 200
 
 
 @app.route("/api/recommend", methods=["POST"])
 def get_recommendation():
-    check_update_file()
     client_songs_list = request.get_json()['songs']
     recommendation = [
         rule[1] for rule in app.model if (rule[0] in client_songs_list and rule[2] >= 0.6)
