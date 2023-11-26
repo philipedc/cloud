f1c71a3636de8cb2036da58992e1fa9489d24045
philipedc <philipedutra2002@gmail.com>
2023-11-26 14:12:16 -0300

    trigger

--- TP2/server/app.py
+++ TP2/server/app.py
@@ -14,7 +14,7 @@ app.model = pickle.load(open(file_path, "rb"))
 
 @app.route("/", methods=["GET"])
 def hello_screen():
-    return jsonify("Hello!!!! Please go to /api/recommend to get a playlist recommendation!"), 200
+    return jsonify("Hi! Please go to /api/recommend to get a playlist recommendation!"), 200
 
 @app.route("/api/recommend", methods=["POST"])
 def get_recommendation():
