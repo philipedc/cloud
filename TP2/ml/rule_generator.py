1383214df4c5bb6c78ed0f6212f49c5f1f485575
Cesar de Paula Morais <cesarmorais@adimplere.com.br>
2023-11-27 12:03:36 -0300

    Added dataset gathering through URL

--- TP2/ml/rule_generator.py
+++ TP2/ml/rule_generator.py
@@ -1,39 +1,36 @@
-# import pandas as pd
-# import pickle
-# from fpgrowth_py import fpgrowth
+import os
+import ssl
+import pandas as pd
+import pickle
+from fpgrowth_py import fpgrowth
 
-# # Change path when on VM
-# FILEPATH_DS1 = './2023_spotify_ds1.csv'
-# FILEPATH_DS1 = './2023_spotify_ds1.csv'
+ssl._create_default_https_context = ssl._create_unverified_context
+DATASET_URL = os.environ.get('DATASET_URL')
 
-# RULES_PATH = './'
+RULES_PATH = './rules/'
 
-# def generate_rules(df):
-#     df = df.loc[:, ['pid', 'track_name', 'artist_name']]
-#     df.dropna()
-#     grouped_df = df.groupby('pid').agg({'track_name': list, 'artist_name': list}).reset_index()
+def generate_rules(df):
+    df = df.loc[:, ['pid', 'track_name', 'artist_name']]
+    df.dropna()
+    grouped_df = df.groupby('pid').agg({'track_name': list, 'artist_name': list}).reset_index()
+    item_set_list = grouped_df['track_name'].values.tolist()
+  
+    freqItemSet, rules = fpgrowth(item_set_list, minSupRatio=0.07, minConf=0.5)
+  
+    # Format rules
+    formatted_rules = []
+    for rule in rules:
+        formatted_rules.append([str(list(rule[0])[0]), str(list(rule[1])[0]), rule[2]])
+  
+    return formatted_rules
+  
+df = pd.read_csv(DATASET_URL)
 
-#     item_set_list = grouped_df['track_name'].values.tolist()
+basic_rules = generate_rules(df)
+
+with open(RULES_PATH + "basic_rules.pkl", 'wb+') as f:
+    pickle.dump(basic_rules, f)
     
-#     freqItemSet, rules = fpgrowth(item_set_list, minSupRatio=0.07, minConf=0.5)
-    
-#     # Format rules
-#     formatted_rules = []
-
-#     for rule in rules:
-#         formatted_rules.append([str(list(rule[0])[0]), str(list(rule[1])[0]), rule[2]])
-    
-#     return formatted_rules
-    
-# first_df = pd.read_csv(FILEPATH_DS1)
-# second_df = pd.read_csv(FILEPATH_DS1)
-
-# improved_df = pd.concat([first_df, second_df], ignore_index=True)
-
-# basic_rules = generate_rules(first_df)
-# with open(RULES_PATH + "basic_rules.pkl", 'wb+') as f:
-#     pickle.dump(basic_rules, f)
-
-# #improved_rules = generate_rules(improved_df)
-# #with open(RULES_PATH + "improved_rules.pkl", 'wb+') as f:
-# #    pickle.dump(improved_rules, f)
+# Debug
+## model = pickle.load(open(RULES_PATH + "basic_rules.pkl", "rb"))
+## print(f"After pickle: {model[:10]}")
